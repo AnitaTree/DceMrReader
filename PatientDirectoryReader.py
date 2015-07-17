057a33b4b52b944a79319f3983db0f2d5c59e444
anitabanerji <a.banerji@leeds.ac.uk>
2015-07-17 17:13:28 +0100

    Commenting and tidying up.

--- PatientDirectoryReader.py
+++ PatientDirectoryReader.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from collections import Counter
 
 class PatientDirectoryReader(object):
-
+    """ Responsible for reading and storing information about image files and series. """
     def __init__(self, dirNm):
         """ Set up storage for series information. """
         self._dirNm = dirNm
@@ -21,6 +21,70 @@ class PatientDirectoryReader(object):
         # only keep one set of data at a time for memory purposes
         self._data = None
         self._dataSuid = None
+
+    def getImageData(self, protName):
+        """ Return the image data for a particular series. """
+        suid= self._suidAndTimeForProtocols[protName][0]
+        if self._dataSuid != suid:
+            self._setSeriesInfoAndData(suid)
+        return self._data
+
+    def getOrderedFileList(self, protName):
+        """ Return an ordered list of the files in a series. """
+        suid= self._suidAndTimeForProtocols[protName][0]
+        if suid not in self._fileInfoForSuid:
+            self._setSeriesInfoAndData(suid)
+        infoSorted = self._fileInfoForSuid[suid]
+        fileList = []
+        for i, file in enumerate(infoSorted):
+            fileList.append(file[0])
+        return fileList
+
+    def getSequenceParameters(self, protName):
+        """ Get the matrix and time dimensions af the series. """
+        if protName not in self._sequenceParameters:
+            fileInfo= self._fileInfoForSuid[self._suidAndTimeForProtocols[protName][0]]
+            dcm = dicom.read_file(os.path.join(self._dirNm, fileInfo[0][0]), stop_before_pixels=True, force= True)
+            acquisitionTimes= []
+            sliceLocations= []
+            for file in fileInfo:
+                acquisitionTimes.append(file[1])
+                sliceLocations.append(file[2])
+            if self._getFileType(dcm) != 'enhancedDICOM':
+                nt = Counter(sliceLocations)[sliceLocations[0]]
+                nz = Counter(acquisitionTimes)[acquisitionTimes[0]]
+                # for unusual cases e.g. bold
+                if nz*nt > len(fileInfo):
+                    nt= len(fileInfo)/nz
+            else:
+                nf = dcm.NumberOfFrames
+                nz= dcm.PerFrameFunctionalGroupsSequence[nf-1].FrameContentSequence[0].DimensionIndexValues[2]
+                # could get the nt from the DimensionIndexValues[3] array for a 4D image, but for a 3D image this
+                # will not exist
+                nt= nf / nz
+            self._sequenceParameters[protName]= [dcm.Rows, dcm.Columns, nz, nt]
+        return self._sequenceParameters[protName]
+
+    def getSeriesNames(self):
+        """ Return and ordered list of the series. """
+        return self._protNamesOrdered
+
+    def _getFileInfoAndData(self, file):
+        """ Get info about the file and the pixel data.
+
+            Assumes all the files are safe to read.
+        """
+        dcm= dicom.read_file(file, stop_before_pixels=False, force=True)
+        if not dcm.dir('SamplesPerPixel'):
+            #required for NEMA for the pixel_array access to work
+            dcm.SamplesPerPixel = 1
+        if 'SliceLocation' in dcm:
+            sliceLocation = dcm.SliceLocation
+        else:
+            sliceLocation = 0
+        imageData = dcm.pixel_array
+        fileInfo = [file, self._getImageTime(dcm), sliceLocation]
+        return fileInfo, imageData
 
     def _getFileType(self, dcm):
         """ Check whether the file is DICOM, multiframe DICOM or NEMA."""
@@ -53,23 +117,6 @@ class PatientDirectoryReader(object):
         except ValueError:
             time = float(acqTime)
         return time
-
-    def _getFileInfoAndData(self, file):
-        """ Get info about the file and the pixel data.
-
-            Assumes all the files are safe to read.
-        """
-        dcm= dicom.read_file(file, stop_before_pixels=False, force=True)
-        if not dcm.dir('SamplesPerPixel'):
-            #required for NEMA for the pixel_array access to work
-            dcm.SamplesPerPixel = 1
-        if 'SliceLocation' in dcm:
-            sliceLocation = dcm.SliceLocation
-        else:
-            sliceLocation = 0
-        imageData = dcm.pixel_array
-        fileInfo = [file, self._getImageTime(dcm), sliceLocation]
-        return fileInfo, imageData
 
     def _setSeriesInfoAndData(self, suid):
         """ Fill the class data structures the hold the information and Data for the SUID. """
@@ -104,47 +151,3 @@ class PatientDirectoryReader(object):
             self._fileInfoForSuid[suid] = seriesFileInfo
             self._data = seriesData
         self._dataSuid = suid
-
-    def getSequenceParameters(self, protName):
-        """ Get the matrix and time dimensions af the series. """
-        if protName not in self._sequenceParameters:
-            fileInfo= self._fileInfoForSuid[self._suidAndTimeForProtocols[protName][0]]
-            dcm = dicom.read_file(os.path.join(self._dirNm, fileInfo[0][0]), stop_before_pixels=True, force= True)
-            acquisitionTimes= []
-            sliceLocations= []
-            for file in fileInfo:
-                acquisitionTimes.append(file[1])
-                sliceLocations.append(file[2])
-            if self._getFileType(dcm) != 'enhancedDICOM':
-                nt = Counter(sliceLocations)[sliceLocations[0]]
-                nz = Counter(acquisitionTimes)[acquisitionTimes[0]]
-                # for unusual cases e.g. bold
-                if nz*nt > len(fileInfo):
-                    nt= len(fileInfo)/nz
-            else:
-                nf = dcm.NumberOfFrames
-                nz= dcm.PerFrameFunctionalGroupsSequence[nf-1].FrameContentSequence[0].DimensionIndexValues[2]
-                # could get the nt from the DimensionIndexValues[3] array for a 4D image, but for a 3D image this
-                # will not exist
-                nt= nf / nz
-            self._sequenceParameters[protName]= [dcm.Rows, dcm.Columns, nz, nt]
-        return self._sequenceParameters[protName]
-
-    def getSeriesNames(self):
-        return self._protNamesOrdered
-
-    def getOrderedFileList(self, protName):
-        suid= self._suidAndTimeForProtocols[protName][0]
-        if suid not in self._fileInfoForSuid:
-            self._setSeriesInfoAndData(suid)
-        infoSorted = self._fileInfoForSuid[suid]
-        fileList = []
-        for i, file in enumerate(infoSorted):
-            fileList.append(file[0])
-        return fileList
-
-    def getImageData(self, protName):
-        suid= self._suidAndTimeForProtocols[protName][0]
-        if self._dataSuid != suid:
-            self._setSeriesInfoAndData(suid)
-        return self._data
